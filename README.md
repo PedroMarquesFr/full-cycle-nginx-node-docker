c2b5a970544ec6f6015f8138d2053e860e20ac06
Your Name <you@example.com>
2022-07-15 11:27:51 -0300

    fix: removed mysql file from track without breaking project

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-## Ola pessoa que ta corrigindo minha entrega
+## Ola  
 
-### Tenho duas perguntas
+### Ainda tenho uma duvida
 
-Na descricao pede pra que tudo rode so com o docker-compose up -d mas por algum motivo o comando CMD ["node", "index.js"] no container do node nao é executado, para rodar precisa entrar no container `docker exec -it app bash` e rodar `node index.js` manualmente, pode me ajudar com isso?
-
-No caso das infos do volume do mysql, qual a melhor forma de armazena-las no github sem acupar muito espaco ao mesmo tempo que mantem os arquivos fundamentais das tabelas? Oq devo colocar no gitignore/dockerignore?
+Na descricao pede pra que tudo rode so com o docker-compose up -d mas por algum motivo o comando CMD ["node", "index.js"] no container do node nao é executado, para rodar precisa entrar no container `docker exec -it app bash` e rodar `node index.js` manualmente, nao sei oq ta rolando, pode me ajudar com isso?
--- node/index.js
+++ node/index.js
@@ -8,7 +8,6 @@ const config = {
   host: "db",
   user: "root",
   password: "root",
-  database: "nodedb",
 };
 
 app.get("/", (req, res) => {
@@ -16,15 +15,23 @@ app.get("/", (req, res) => {
   const sqlInsert = `INSERT INTO people(name) values ('${random.first()}')`;
   const sql = "SELECT * FROM people";
 
-  connection.query(sqlInsert);
-  connection.query(sql, function (err, result, fields) {
-    const namesList = result.reduce((lists, row) => {
-      return lists + `<li>${row.name.toString()}</li>`;
-    }, "");
-    connection.end();
-    res.send(`<h1>Full Cycle Rocks!</h1> <ul>${namesList}</ul>`);
+  connection.query("CREATE DATABASE nodedb", (err, result) => {
+    connection.query("USE nodedb", (err, result) => {
+      connection.query(
+        "CREATE TABLE  people(id int not null auto_increment, name varchar(255), primary key(id))",
+        (err, result) => {
+          connection.query(sqlInsert);
+          connection.query(sql, function (err, result, fields) {
+            const namesList = result.reduce((lists, row) => {
+              return lists + `<li>${row.name.toString()}</li>`;
+            }, "");
+            connection.end();
+            res.send(`<h1>Full Cycle Rocks!</h1> <ul>${namesList}</ul>`);
+          });
+        }
+      );
+    });
   });
-
 });
 
 app.listen(PORT, () => {
